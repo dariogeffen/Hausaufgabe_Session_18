13c021075ccd61164b27a312f653db4dffcf1679
dariogeffen <dariogeffen@web.de>
2020-12-14 16:52:42 +0100

    secuuuurity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 from models import User, db
-import random
+import random, uuid, hashlib
 
 app = Flask(__name__)
 db.create_all()
@@ -10,30 +10,66 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/logged_in", methods=["POST"])
-def logged_in():
+@app.route("/signed_up", methods=["POST"])
+def signed_up():
     name = request.form.get("user-name")
     email = request.form.get("user-email")
+    password = request.form.get("user-password")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     secret_number = random.randint(1, 30)
 
-    user = User(name=name, email=email,secret_number=secret_number)
+    user = db.query(User).filter_by(email=email).first()
 
+    if not user:
+        # create a User object
+        session_token = str(uuid.uuid4())
+        user = User(name=name, email=email, secret_number=secret_number, password=hashed_password, session_token=session_token)
+        # save the user object into a database
+        db.add(user)
+        db.commit()
+        response = make_response(redirect(url_for('game')))
+        response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
+        return response
+    else:
+        return "This user already exists, please log in to access your account."
+
+
+
+@app.route("/logged_in", methods=["POST"])
+def logged_in():
+    email = request.form.get("user-email")
+    password = request.form.get("user-password")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    secret_number = random.randint(1, 30)
+    user = db.query(User).filter_by(email=email).first()
+
+    if hashed_password != user.password:
+        return "WRONG PASSWORD! Go back and try again."
+    elif hashed_password == user.password:
+        # create a random session token for this user
+        session_token = str(uuid.uuid4())
+
+    user.secret_number = secret_number
+    user.session_token = session_token
     db.add(user)
     db.commit()
+
+    name = user.name
     response = make_response(redirect(url_for('game')))
-    response.set_cookie("email", email)
-    response.set_cookie("name", name)
+    response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
     return response
 
 @app.route("/game", methods=["GET", "POST"])
 def game():
     if request.method == "GET":
-        name = request.cookies.get("name")
+        session_token = request.cookies.get("session_token")
+        user = db.query(User).filter_by(session_token=session_token).first()
+        name = user.name
         return render_template("index.html", name=name)
     elif request.method == "POST":
-        email_address = request.cookies.get("email")
-        name = request.cookies.get("name")
-        user = db.query(User).filter_by(email=email_address).first()
+        session_token = request.cookies.get("session_token")
+        user = db.query(User).filter_by(session_token=session_token).first()
+        name = user.name
         secret = str(user.secret_number)
         guess = str(request.form.get("number"))
         if guess == secret:
@@ -60,8 +96,8 @@ def success(guess):
 
 @app.route("/newgame", methods=["GET", "POST"])
 def newgame():
-    email_address = request.cookies.get("email")
-    user = db.query(User).filter_by(email=email_address).first()
+    session_token = request.cookies.get("session_token")
+    user = db.query(User).filter_by(session_token=session_token).first()
     new_secret = random.randint(1, 30)
     user.secret_number = new_secret
     db.add(user)
--- models.py
+++ models.py
@@ -8,4 +8,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    session_token = db.Column(db.String)
     secret_number = db.Column(db.Integer)
